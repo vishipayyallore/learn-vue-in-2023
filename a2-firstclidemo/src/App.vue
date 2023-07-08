c9cfd7d87ffc8285b8fc0ffee42083b08ae37104
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-08 20:19:11 +0530

    Renaming the component

--- a2-firstclidemo/src/App.vue
+++ a2-firstclidemo/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to first Vue.js App" />
-  <NewOne />
+  <FirstOne />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import NewOne from "./NewOne.vue";
+import FirstOne from "./components/FirstOne.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    NewOne
+    FirstOne
   },
 };
 </script>
