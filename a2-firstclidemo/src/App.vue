8a804abd51ceb02349db32a674b151bc04e1b1f6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-08 19:55:41 +0530

    New Component

--- a2-firstclidemo/src/App.vue
+++ a2-firstclidemo/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to first Vue.js App" />
+  <NewOne />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import NewOne from "./NewOne.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    NewOne
   },
 };
 </script>
---
+++ a2-firstclidemo/src/NewOne.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello from NewOne Component !</h1>
+</template>
+<script>
+export default {
+    name: "NewOne",
+};
+</script>
+
+<style scoped>
+h1 {
+    color: rgb(7, 94, 160);
+    }
+</style>
