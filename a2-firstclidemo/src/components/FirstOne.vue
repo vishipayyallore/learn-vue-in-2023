847cd2ac23bd0c34b364924bb343d7cdf135cb68
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-08 20:22:29 +0530

    Interpolation

--- a2-firstclidemo/src/components/FirstOne.vue
+++ a2-firstclidemo/src/components/FirstOne.vue
@@ -1,21 +1,9 @@
 <template>
-    <h1>NewOne {{ email }} | {{ mobile }} | {{ 10 + 10 }} | {{ "Hello World".length }}</h1>
-    <h1>Name: {{ getName() }} | {{ getName("Sri Vari") }}</h1>
-    <h1>Changing Value {{ mobile = 9010050445 }} </h1>
-    <h1>Boolean {{ mobile == 9010050445 }} </h1>
+    <h1>FirstOne Component</h1>
 </template>
 <script>
 export default {
     name: "NewOne",
-    data: function () {
-        return {
-            email: "srivaru@god.com",
-            mobile: "1234567890",
-            getName: function (name) {
-                return name || "Srivaru";
-            },
-        };
-    },
 };
 </script>
 
---
+++ a2-firstclidemo/src/components/Interpolation.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>NewOne {{ email }} | {{ mobile }} | {{ 10 + 10 }} | {{ "Hello World".length }}</h2>
+    <h2>Name: {{ getName() }} | {{ getName("Sri Vari") }}</h2>
+    <h2>Changing Value {{ mobile = 9010050445 }} </h2>
+    <h2>Boolean {{ mobile == 9010050445 }} </h2>
+</template>
+<script>
+export default {
+    name: "NewOne",
+    data: function () {
+        return {
+            email: "srivaru@god.com",
+            mobile: "1234567890",
+            getName: function (name) {
+                return name || "Srivaru";
+            },
+        };
+    },
+};
+</script>
+
+<style scoped>
+h2 {
+    color: rgb(4, 130, 225);
+}
+</style>
