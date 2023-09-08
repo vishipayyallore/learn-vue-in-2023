2e19cf5c89727eb0fcfd95ab12f5a4467259f3aa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-08 23:26:32 +0530

    Changes

--- a2-firstclidemo/src/App.vue
+++ a2-firstclidemo/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to first Vue.js App" />
-  <A1FirstOne />
-  <A2Interpolation />
+  <A4EventsParameters />
   <A3MethodsParameters />
+  <A2Interpolation />
+  <A1FirstOne />
 </template>
 
 <script>
@@ -11,6 +12,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 import A1FirstOne from "./components/A1FirstOne.vue";
 import A2Interpolation from "./components/A2Interpolation.vue";
 import A3MethodsParameters from "./components/A3MethodsParameters.vue";
+import A4EventsParameters from "./components/A4EventsParameters.vue";
 
 export default {
   name: "App",
@@ -19,6 +21,7 @@ export default {
     A1FirstOne,
     A2Interpolation,
     A3MethodsParameters,
+    A4EventsParameters,
   },
 };
 </script>
