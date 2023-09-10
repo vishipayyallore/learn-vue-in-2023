38f76f53feee213be01084374a1d77d7f139019a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-10 16:17:28 +0530

    Update A4EventsParameters.vue

--- a2-firstclidemo/src/components/A4EventsParameters.vue
+++ a2-firstclidemo/src/components/A4EventsParameters.vue
@@ -45,20 +45,22 @@ h3 {
     background-color: #007bff;
     color: #fff;
     border: 1px solid #01376f;
-    /* Add a border with color #01376f */
     padding: 10px 20px;
     cursor: pointer;
     margin-right: 10px;
     border-radius: 5px;
-    /* Add rounded corners */
     box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-    /* Add a box shadow */
     font-weight: bold;
-    /* Make the text bold */
-    transition: background-color 0.3s; /* Add transition for smooth color change */
+    transition: background-color 0.3s, transform 0.1s;
+    /* Add transition for color and transform */
 }
 
 .custom-button:hover {
-  background-color: #0056b3; /* Change background color on hover */
+    background-color: #0056b3;
+}
+
+.custom-button:active {
+    transform: translateY(2px);
+    /* Add a slight push effect on click */
 }
 </style>
