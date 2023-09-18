9e4bea8af16d4e2ee9b49468e1c5744289dfa884
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-18 21:21:54 +0530

    Update A7IfElseDemo.vue

--- a2-firstclidemo/src/components/A7IfElseDemo.vue
+++ a2-firstclidemo/src/components/A7IfElseDemo.vue
@@ -1,6 +1,8 @@
 <template>
-    <h1 id="pages2" v-if="display">If Else Condition - display </h1>
-    <h1 id="pages2" v-if="show">If Else Condition - show </h1>
+    <h1 id="pages2" v-if="display">If Condition - display </h1>
+    <h1 id="pages2" v-else>Else Condition - display </h1>
+
+    <h1 id="h1show" v-if="show">If Else Condition - show </h1>
 </template>
   
 <script>
@@ -18,6 +20,13 @@ export default {
 <style scoped>
 #pages2 {
     background-color: #42b983;
+    color: #ffffff;
+    padding: 10px;
+    border-radius: 5px;
+}
+
+#h1show {
+    background-color: #0272bd;
     color: #ffffff;
     padding: 10px;
     border-radius: 5px;
